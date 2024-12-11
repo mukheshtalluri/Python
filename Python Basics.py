00b0c15f43ea83d6c70a1cc3dd0b8a4277be76d6
Mukhesh <mukheshtalluri25@gmail.com>
2024-12-11 22:16:28 +0530

    Python Training

--- Python Basics.py
+++ Python Basics.py
@@ -124,6 +124,81 @@ Keyword arbitrary arguments: It will work same as the key word arguments the dif
 Return : Return statement used to store the value in the variable, and we can use many other purpose. If we want to use
 value in other function we can store in return statement will use.
 
+List: List is ordered, mutable data structure which will allow multiple data types and allow duplicates. Which is used to store
+multiple values in the single variable. Since lists are ordered it will support for the indexing, and we have many methods
+to perform operations on list.
+List comprehension: We can create the list in a single line with list comprehension.
+
+Tuple: Tuple is order, immutable data structure which will allow multiple data types and allow duplicates.Since tuple is immutable
+which bit faster compare with the list, and we can perform limited operations.
+
+Set: Set is unordered, mutable data structure it won't allow duplicates. We have several methods to perform operations on set.
+
+Dictionary: Dictionary will contain key value pairs and all keys are unique. There are ordered in nature.
+
+f-strings : f-strings are help us to format the string while concatenating. If we want to print any other data type in side
+string it will through type error. To aviod and smooth work progress will use f-strings.
+
+Doc strings: Doc strings will help us while working with the functions we need give information to others about function
+will use doc strings and doc strings will write after the function.
+
+Recursion: Recursion is the function calling itself until base condition will execute. Recursion will work as function
+calling itself with small change in input. Recursion works with the Stack implementation.
+
+Exception - handling: Exception is process of handling unwanted or unknown events to be occurred. If we or not handling
+errors it will lead to the program crash. In python, we handle errors with the try-except block.
+
+Custom Exception: Using raise keyword we can use the custom exception.
+
+Define custom Exception: We can create exceptions as per requirement using Exception class. Exception class need to be
+inherited from there we can create custom exceptions.
+
+Shorthand if else statement: It works same as the if else condition. In the traditional if else condition we need to use
+indentation and multiple lines of code when it comes to shorthand if else condition we can define everything in one line.
+
+Enumerate : Enumerate function will useful when we are working with the loops. sometimes we required value along with the
+index in this condition enumerate function will generate the index for us based on that we can link index value with their value.
+
+Virtual environment: Virtual environment is the place where we can isolate everything from the global python. When we are
+working on multiple projects due constrains we need to use different python version in these scenario python will help
+us a lot. we can create the virtual environment for the every project and we can work on that and it is easy to change version of
+any dependency as well.
+Some commands from virtual environment:
+python -m venv myenv --> In place of myenv we need to provide the virtual environment name.
+myenv\Scripts\activate.bat --> Before using we need to activate the virtual environment this command will activate the virtual environment.
+deactivate --> This will deactivate the virtual environment.
+pip freeze > requirements.txt --> It will give the list of packages and their version in virtual environment
+pip install -r requirements.txt --> This command used to install all those virtual environment related packages in the others machine.
+
+Import function: When we want to use any other module which is not created by us. we need to import particular module
+and we can use that module. There are several ways to import to packages we discuss them
+import math --> when we want to use any methods we can call - math.sqrt()
+import math as m --> in this scenario will use as the m.sqrt()
+from math import sqrt --> here we directly we use the method sqrt()
+from math import sqrt, pi --> we can import multiple methods as well
+from math import * --> It will import all methods which were available in that package.
+dir function --> dir function will show the all methods which are present in that package
+
+if __name__ == "__main__" : This function will help us find the method is running directly or imported from other module
+
+OS Module: Os module is the system module which we can use when we want to perform some operations on the os level we
+can use this module.
+
+Local vs Global: If we are define a variable in side the function the scope of function will be inside the function only
+outside of function we cannot be able to access the variable this variable we call it as the local variable. Variable define
+at the outside of the function, and we can access throughout program  this kind of variable we can call it as the global variable.
+
+Operations on files: When we want to perform operations on file we will perform FileI/O operations.There are few methods
+are there to working with the file files read, write, append, create, binary.
+
+Lambda function: Lambda is small anonymous function it will take many inputs and will give the one output. lambda function work as
+like function it will useful for the small functions not for the complex functions.
+
+Is vs == : Is will compare the memory address of the values in case of == will compare the value comparison.
+
+Map, filter, Reduce : Map function is used to map the function with the iterable, It will map with all values in a iterable
+and the object will return by map object same length as iterable. Filter function is used to filter some of the values
+based on condition which were defined in function. Reduce function will take min 2 inputs any no.of inputs finally it will return one value.
 
 """
 # Using internal module
@@ -567,6 +642,332 @@ def addition(a, b):
 
 result = addition(5, 6)
 print(result)
+
+# List
+names = ["Cat", "Tiger", "Lion", "Puma", "Jaguar", "Dog", "Cheetah"]
+for name in names:
+    print(name)
+
+# List index
+print(names[2])
+
+# List methods
+# Append - add value at the end.
+names.append("Elephant")
+
+# Insert - insert method will add the element at the given index
+names.insert(2, "Rabbit")
+
+# Pop - pop method will remove element at the end, if we pass any index as argument it will remove that index element.
+names.pop()
+
+# Remove - remove method will remove element based on the value
+names.remove("Dog")
+
+# Count - count method will count the no of times element in the list.
+names.count("Lion")
+
+# Index - index method will return index of element.
+names.index("Lion")
+
+# Sort - sort method will sort the elements ascending order.
+names.sort()
+
+# Reverse - reverse method will reverse the list
+names.reverse()
+
+# Extend - extend method is used to adding an iterable to list.
+name = ["Fox"]
+names.extend(name)
+
+# Copy - copy method is used to copy elements from one list other.
+name1 = names.copy()
+# There are two types of copying will there copy method will refer to shallow copy and and we need to perform deep copy
+name2 = names
+
+# Clear - clear method will remove all elements from the list.
+names.clear()
+
+# List comprehension
+even_numbers = [x for x in range(1, 11) if x % 2 == 0]
+print(even_numbers)
+
+
+# Tuples
+fruits = ("Mango", "Apple", "Papaya", "Orange", "Banana")
+
+# Tuple methods
+# Index - index method will use to find the index of element.
+fruits.index("Apple")
+
+# Count - count method will count the no of occurrence of the element.
+fruits.count("Apple")
+
+
+# Sets - Sets are unordered there is no index concept in set.
+animals = {"Cow", "Goat", "Ox", "Buffalo", "Sheep", "Donkey"}
+
+# Set methods
+# Add - add method will add elements to the set
+animals.add("Horse")
+
+# Remove - remove method will remove element based on argument, It element no there it will raise exception.
+animals.remove("Cow")
+
+# Discard - Discord method will also used to remove the element from the set, If element not there then also it won't raise any exception.
+animals.discard("Animal")
+
+# Pop - pop method will remove element randomly.
+animals.pop()
+
+# Update - update method is used update one set with the other
+birds = {"Peacock", "Parrot", "Hen", "Crow", "Owl"}
+animals.update(birds)
+
+# Copy - copy method is used to copy the elements of the set
+
+# Clear - clear method will remove elements from the set
+animals.clear()
+
+odd_numbers = {1, 3, 5, 7, 9, 11} # set1
+prime_numbers = {1, 2, 3, 5, 7, 11} # set2
+
+# Difference - difference method will return set1 elements which are not in set2
+odd_numbers.difference(prime_numbers)
+
+# Intersection - intersection method will help us to return elements which are present in both set1 and set2
+odd_numbers.intersection(prime_numbers)
+
+# Union - It will return all elements which are present in the both the sets
+odd_numbers.union(prime_numbers)
+
+# Isdisjoint - Both sets dont have any common elements it will return True else False
+odd_numbers.isdisjoint(prime_numbers)
+
+# Issubset - All elements in set1 in set2
+odd_numbers.issubset(prime_numbers)
+
+# Issuperset - All elements in set2 in set1
+odd_numbers.issuperset(prime_numbers)
+
+# Symmetric difference - It will show elements which are not common in both the sets
+odd_numbers.symmetric_difference(prime_numbers)
+
+# difference update -
+odd_numbers.difference_update(prime_numbers)
+
+# intersection update -
+odd_numbers.intersection_update(prime_numbers)
+
+# Symmetric difference update
+odd_numbers.symmetric_difference_update(prime_numbers)
+
+
+# Dictionary
+person_info = {"name": "Mukhesh", "Age": 25, "Profession": "Software Engineer", "Language": "Python"}
+for key, value in person_info.items():
+    print(f"{key} : {value}")
+
+# Dictionary methods
+# Keys - It will return all keys in dictionary
+print(person_info.keys())
+
+# Values - It will return all values in the dictionary
+print(person_info.values())
+
+# Items - It will return both the key and values from the dictionary
+for key, value in person_info.items():
+    print(f"{key} : {value}")
+
+# Copy - Copy method will copy the elements from the dictionary
+person_info.copy()
+
+# Update - Update method will used to update with the other dictionary
+my_dict = {}
+person_info.update(my_dict)
+
+# Pop - Pop method is used to pop the item based on the key value.
+person_info.pop("Age")
+
+# Popitem - Popitem is used to remove element which is present at the end.
+person_info.popitem()
+
+# Setdefault - Setdefault is used to make any value will be default value
+person_info.setdefault("Country", "India")
+
+# Clear - Clear method is used to clear all the elements from the dictionary
+person_info.clear()
+
+
+# F-Strings - Will use f before writing the string it will identify other data types and will convert to string.
+name = "Python"
+age = 33
+print(f"My name is {name} and i am {age} year's old.")
+
+
+# Doc string - While calling function will mouse hover the function it will display the doc string.
+def addition(a, b):
+    """The above function will add the both the numbers."""
+    return a + b
+print(addition(5, 6))
+
+# Recursion
+def first_method():
+    second_method()
+    print("First method executed.")
+
+def second_method():
+    third_method()
+    print("Second method executed.")
+
+def third_method():
+    fourth_method()
+    print("Third method executed.")
+
+def fourth_method():
+    print("Fourth method executed.")
+
+first_method()
+
+
+# Exception handling
+try:
+    num1 = int(input("Enter the first num : "))
+    num2 = int(input("Enter the second num : "))
+    result = num1 / num2
+except ZeroDivisionError:
+    print("Number cannot be divided by zero.")
+except ValueError:
+    print("Invalid input. Enter valid input.")
+else:
+    print("The division of above two numbers is :", result)
+finally:
+    print("Execution complete")
+
+
+# Custom exception
+age = int(input("Enter your age: "))
+if age < 18:
+    raise ValueError("You are not eligible for the cast to vote.")
+
+# Define custom exception
+class NegativeNumberError(Exception):
+    def __init__(self, message="Negative numbers are not allowed"):
+        self.message = message
+        super().__init__(self.message)
+
+
+# Shorthand if else condition
+num = 10
+print("Even number" if num % 2 == 0 else "Odd number")
+
+# Enumerate function
+for i, val in enumerate(range(1, 12)):
+    print(i, val)
+
+
+# Local vs Global --> y is the global variable and x is the local variable. We can change global variable inside function using global keyword.
+y = 7
+def local_vs_global():
+    x = 5
+    print(x)
+local_vs_global()
+print(y)
+
+
+# File operations
+# Create file - if file already exists it will throw error (FileExistError)
+f = open("file.txt", 'x')
+f.close()
+
+# Write file - If we want to write something into the file we can use write option. In case write if file not there it will create new file
+f = open("file.txt", "w")
+f.write("Hello everyone...!")
+f.close()
+
+# Read file - Reading contents from the existing file.
+f = open("file.txt", "r")
+content = f.read()
+f.close()
+
+# Append - If we write something it will replace the existing text to aviod that we can use append method to add text at the end.
+f = open("file.txt", "a")
+f.write("How are you doing...!")
+f.close()
+
+# binary - When we want to deal with the binary data in that condition we can use binary data.
+f = open("file.txt", "b")
+f.close()
+
+# with key word
+with open("file.txt", "w") as file:
+    file.write("Here no need to do any file operation it will do everything on behalf of us.")
+
+# There are few more operations also available on file will discuss.
+# Readline, Readlines, Writelines, seek, tell, truncate.
+
+
+# Lambda function - we can make this function as per requirement.
+func = lambda x: x ** 2
+print(func(2))
+
+
+# Is vs ==
+a, b = 5, 5
+print(a == b)
+print(a is b)
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)
+print(a is b)
+
+
+# Reference function
+def even_number(num):
+    if num % 2 == 0:
+        return "Even number"
+    return "Odd number"
+
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Map function
+print(list(map(even_number, test_list)))
+
+def even_number(num):
+    if num % 2 == 0:
+        return num
+
+# Filter function
+print(list(filter(even_number, test_list)))
+
+# Reduce function
+from functools import reduce
+
+def addition(x, y):
+    return x + y
+
+print(reduce(addition, test_list))
+
+# Higher order function - Function which will take other function as a input those functions will call it as the higher order functions.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
